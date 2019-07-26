db37a8a83d107caae41e1ed060a438c6d913f735
Alejandro Castro <guardiadelcastro@gmail.com>
2019-07-26 15:03:56 +0100

    Navbar added

---
+++ components/NavigationBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class="flex justify-between px-5 py-5 bg-gray-900">
+    <div class="flex">
+      <h3 class="text-gray-200 font-bold uppercase tracking-wide">Nuxt Cinema</h3>
+    </div>
+    <div>
+      <button class="flex">
+        <svg class="fill-current text-gray-200" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"/></svg>
+      </button>
+    </div>
+    <nav class="hidden">
+
+    </nav>
+  </header>
+</template>
+
+<script>
+  export default {
+    name: 'NavigationBar'
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
